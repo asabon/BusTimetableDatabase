5ba959ab1cb75e37ee73c5a3628a21775ef06bc5
Takafumi Asahara <asabon.t@gmail.com>
2024-09-14 10:24:15 +0900

    add check script

---
+++ script/check.py
@@ -0,0 +1,13 @@
+import json
+
+def check_timetable():
+    return 0
+
+def check_companies(dirName):
+    return 0
+
+def check_systems(dirName):
+    return 0
+
+def check_all(rootdir):
+    return 0
