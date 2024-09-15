43718e5528e10610cd5dc4495a69526263d1b5de
Takafumi Asahara <asabon.t@gmail.com>
2024-09-15 17:03:14 +0900

    modify

--- script/check.py
+++ script/check.py
@@ -1,13 +1,20 @@
 import json
 
-def check_timetable():
-    return 0
-
 def check_companies(dirName):
+    companies_json_open = open(dirName + '/companies.json', 'r')
+    companies_json_load = json.load(companies_json_open)
+    for v in companies_json_load.values():
+        print(v)
     return 0
 
 def check_systems(dirName):
     return 0
 
+def check_timetable():
+    return 0
+
 def check_all(rootdir):
+    result_companies = check_companies(rootdir)
+    if result_companies != 0:
+        return result_companies
     return 0
