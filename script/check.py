b0b60d08ba2cb0da69f12c448bb92609dd176dfa
Takafumi Asahara <asabon.t@gmail.com>
2024-09-27 05:04:16 +0900

    modify check script

--- script/check.py
+++ script/check.py
@@ -19,9 +19,22 @@ def check_company(rootdir, companies_json):
     print (" OK")
     return 0
 
-def get_route_dirs(company_dir):
+def get_route_files(company_dir):
     print("get_route_dirs(" + company_dir + ")")
-    print(glob.glob(company_dir + '/**/route.json', recursive=True))
+    route_files = glob.glob(company_dir + '/**/route.json', recursive=True)
+    print(" " + route_files)
+    return route_files
+
+def check_route_file(route_file):
+    route_text = open(route_file, 'r', encoding='utf-8')
+    route_json = json.load(route_text)
+    for station in route_json['stations']:
+        if station['file'] != "":
+            station_file = os.path.split(route_file)[0] + '/' + station['file']
+            if (os.path.isfile(station_file) != True):
+                print(' ' + station_file + ' is NOT exist. ERROR')
+                return -1
+    return 0
 
 def check_result(result):
     if (result == 0):
@@ -35,7 +48,10 @@ def check_all(rootdir):
     result = check_company(rootdir, companies_json)
     check_result(result)
     for company in companies_json['companies']:
-        get_route_dirs(rootdir + '/' + company['directory'])
+        route_files = get_route_files(rootdir + '/' + company['directory'])
+        for route_file in route_files:
+            result = check_route_file(route_file)
+            check_result(result)
     sys.exit(0)
 
 if __name__ == "__main__":
