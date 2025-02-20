ae4c1c0e12fcd07538ea77836d44bb08db6d2cd7
Takafumi Asahara <asabon.t@gmail.com>
2025-02-21 08:41:53 +0900

    modify

--- script/check_timetable.py
+++ script/check_timetable.py
@@ -1,37 +1,39 @@
 import json
+import sys
+import re
 
-def strtoint(string):
-    return int(string.replace(":", ""))
-
-# timetable.json をチェックする。
-def check_timetable(filePath):
-    with open(filePath, 'r') as file:
-        data = json.load(file)
-        # 空欄ではないかチェック
-        if (data['date'] == ""):
-            return -1
-        if (data['name'] == ""):
-            return -1
-        if (data['position'] == ""):
-            return -1
-        if (data['system'] == ""):
-            return -1
-        # 時刻の順番チェック (weekday)
-        for i in range (1, len(data["weekday"])):
-            before = strtoint(data["weekday"][i-1])
-            after  = strtoint(data["weekday"][i])
-            if before >= after:
-                return -1
-        # 時刻の順番チェック (saturday)
-        for i in range (1, len(data["saturday"])):
-            before = strtoint(data["saturday"][i-1])
-            after  = strtoint(data["saturday"][i])
-            if before >= after:
-                return -1
-        # 時刻の順番チェック (holiday)
-        for i in range (1, len(data["holiday"])):
-            before = strtoint(data["holiday"][i-1])
-            after  = strtoint(data["holiday"][i])
-            if before >= after:
-                return -1
+def check_timetable(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        # Check "date" field
+        result = check_date(data)
+        if result != 0:
+            return result
+    except FileNotFoundError:
+        return 10000001
+    except json.JSONDecodeError:
+        return 10000002
     return 0
+
+def check_date(data):
+    if "date" not in data:
+        # "date" フィールドが存在しなかった
+        return 20000001
+    date_string = data["date"]
+    date_pattern = re.compile(r"^\d{4}/\d{2}/\d{2}$")
+    if date_pattern.match(date_string) == None:
+        # "date" フィールドの値が日付形式ではなかった
+        return 20000002
+    return 0
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python check_timetable.py <file_path>")
+        sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+        result = check_timetable(file_path)
+        if result != 0:
+            print(f"Error: {result}, File: {file_path}")
+        sys.exit(result)
