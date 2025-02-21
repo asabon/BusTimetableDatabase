1f371ea04662019eb000eef471bd367bf0592bb2
Takafumi Asahara <asabon.t@gmail.com>
2025-02-21 09:01:53 +0900

    modify

--- script/check_timetable.py
+++ script/check_timetable.py
@@ -2,12 +2,23 @@ import json
 import sys
 import re
 
+
 def check_timetable(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
         # Check "date" field
         result = check_date(data)
+        if result != 0:
+            print(f"Error: {result}, File: {file_path}")
+            return result
+        # Check "name" field
+        result = check_name(data)
+        if result != 0:
+            print(f"Error: {result}, File: {file_path}")
+            return result
+        # Check "destinations" field
+        result = check_destinations(data)
         if result != 0:
             print(f"Error: {result}, File: {file_path}")
             return result
@@ -20,16 +31,40 @@ def check_timetable(file_path):
     print(f"OK: {file_path}")
     return 0
 
+
+# "date" フィールドのチェック
 def check_date(data):
     if "date" not in data:
         # "date" フィールドが存在しなかった
-        return 20000001
+        return 11000001
     date_string = data["date"]
     date_pattern = re.compile(r"^\d{4}/\d{2}/\d{2}$")
     if date_pattern.match(date_string) == None:
         # "date" フィールドの値が日付形式ではなかった
-        return 20000002
+        return 11000002
     return 0
+
+
+# "name" フィールドのチェック
+def check_name(data):
+    if "name" not in data:
+        # "name" フィールドが存在しなかった
+        return 12000001
+    name_string = data["name"]
+    if "name" == "":
+        # "name" が空だった
+        return 12000002
+    return 0
+
+
+# "destinations" フィールドのチェック
+def check_destinations(data):
+    if "destinations" not in data:
+        # "destinations" フィールドが存在しなかった
+        return 13000001
+    destinations_list = data["destinations"]
+    return 0
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
