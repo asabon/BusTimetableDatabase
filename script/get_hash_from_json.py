d4a43cdf6c434214559cb617491d77b6344efae8
Takafumi Asahara <asabon.t@gmail.com>
2025-04-29 19:25:36 +0900

    modify script

---
+++ script/get_hash_from_json.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+def get_hash_from_json(filepath):
+    with open(filepath, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    hash_value = data.get("hash")
+    return hash_value
+
+if __name__ == "__main__":
+    filepath = sys.argv[1]
+    key = sys.argv[2]
+    hash_value = get_hash_from_json(filepath)
+    print(f"{key}={hash_value}")
