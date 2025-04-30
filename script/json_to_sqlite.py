d9c4e7157d8de6e6b4ae6cf4e065bcfd72b7f9da
Takafumi Asahara <asabon.t@gmail.com>
2025-04-30 15:14:54 +0900

    modify script

--- script/json_to_sqlite.py
+++ script/json_to_sqlite.py
@@ -1,6 +1,7 @@
 import json
 import sqlite3
 import os
+import sys
 
 def create_database(filename):
     conn = sqlite3.connect(filename)
@@ -21,6 +22,9 @@ def create_database(filename):
 
 def json_to_sqlite(root_directory, output_file):
 
+    print("root_directory = " + root_directory)
+    print("output_file    = " + output_file)
+
     # データベース作成
     # cursor = create_database(output_file)
 
@@ -31,3 +35,8 @@ def json_to_sqlite(root_directory, output_file):
             print(file)
             with open("a.json", "r", encoding="utf-8") as file:
                 data = json.load(file)
+
+if __name__ == '__main__':
+    root_directory = sys.argv[1]
+    output_file = sys.argv[2]
+    json_to_sqlite(root_directory, output_file)
