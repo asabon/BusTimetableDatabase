379e72c19ee9853fa144d363d6c1cad3cf4878e6
Takafumi Asahara <asabon.t@gmail.com>
2025-04-30 15:10:01 +0900

    add script to create sqlite file

---
+++ script/json_to_sqlite.py
@@ -0,0 +1,33 @@
+import json
+import sqlite3
+import os
+
+def create_database(filename):
+    conn = sqlite3.connect(filename)
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS timetable (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        position TEXT,
+        system TEXT NOT NULL,
+        destinations TEXT NOT NULL,
+        weekday TEXT,
+        saturday TEXT,
+        holiday TEXT
+    )
+    """)
+    return cursor
+
+def json_to_sqlite(root_directory, output_file):
+
+    # データベース作成
+    # cursor = create_database(output_file)
+
+    sub_directories = os.listdir(root_directory)
+    for sub_directory in sub_directories:
+        files = os.listdir(sub_directory)
+        for file in files:
+            print(file)
+            with open("a.json", "r", encoding="utf-8") as file:
+                data = json.load(file)
