d8e366a4fefdd6ac011b26af95f80f25570328a6
Takafumi Asahara <asabon.t@gmail.com>
2025-07-16 06:39:21 +0900

    update scripts for moved files

--- script/execute.bat
+++ script/execute.bat
@@ -6,7 +6,7 @@ pushd %~dp0
     python v1/generate.py ../database/神奈川中央交通 True
 
     rem v1
-    python v1/update_route.py ../database/kanachu/v1 ../work/busstops.json
-    python v1/generate.py ../database/kanachu/v1 True
+    python v1/update_route.py ../database/kanachu/v1/database ../work/busstops.json
+    python v1/generate.py ../database/kanachu/v1/database True
 
 popd
