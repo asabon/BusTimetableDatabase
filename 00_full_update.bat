60cd3816de77537dcf0fcb2b7c00831d4af87ffe
Takafumi Asahara <asabon.t@gmail.com>
2025-10-07 16:09:13 +0900

    不要コード整理

--- 00_full_update.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 > nul
-
-python .\script\create_template.py .\database\神奈川中央交通 .\work\busstops.json
-python .\script\update_route.py .\database\神奈川中央交通 .\work\busstops.json
-python script/generate.py "database/神奈川中央交通" False
