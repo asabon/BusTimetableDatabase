1293d510b0fc09ab4484bc40eaf5aac1fbfef903
Takafumi Asahara <asabon.t@gmail.com>
2025-11-16 04:57:02 +0900

    v3 追加-2

--- script/v2/main.py
+++ script/v2/main.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import re
 import json
 import logging
 from bs4 import BeautifulSoup
 from urllib.parse import quote
+from collections import deque
 
 logging.basicConfig(
     level=logging.INFO,
@@ -97,6 +99,99 @@ def load_route_ids() -> list[str]:
     
     return route_id_list
 
+# sdid とバス停名から路線IDリストを取得する関数
+def get_route_ids_list(sdid: str, name: str) -> list[str]:
+    route_ids = set()
+    url = f'https://www.kanachu.co.jp/dia/diagram/search?k={quote(name)}&rt=0&t=0&sdid={sdid}'
+    html = get_data(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    for a_tag in soup.find_all('a', href=True):
+        match = re.search(r'/dia/route/index/cid:(\d{10})/', a_tag['href'])
+        if match:
+            route_ids.add(match.group(1))
+    return sorted(route_ids)
+
+# 路線IDからバス停リストを取得する関数
+def get_busstops_list(route_id: str) -> list[str]:
+    url = f"https://www.kanachu.co.jp/dia/route/index/cid:{route_id}/"
+    html = get_data(url)
+    soup = BeautifulSoup(html, "html.parser")
+    system = get_system_from_route_html(html)
+    busstops = []
+    for li in soup.find_all("li", id=re.compile(r"\d+-\d+")):
+        li_id = li.get("id")
+        if not li_id or "-" not in li_id:
+            continue
+
+        busstop_id, busstop_index = li_id.split("-")
+
+        # バス停名の取得
+        span = li.find("span", class_="busStopPoint")
+        busstop_name = span.get_text() if span else ""
+
+        # onClick属性を持つaタグを明示的に探す
+        #a_tag = li.find("a", onClick=re.compile(r"move_center"))
+        a_tag = li.find("a", attrs={"onclick": re.compile(r"move_center")} )
+        lat, lng = None, None
+        if a_tag:
+            on_click = a_tag.get("onclick", "")
+            m = re.search(r"move_center\(([\d.]+),\s*([\d.]+),", on_click)
+            if m:
+                lat, lng = m.group(1), m.group(2)
+        else:
+            print("[Error] tag is NOT found.")
+        busstops.append({
+            "id": busstop_id,
+            "index": busstop_index,
+            "name": busstop_name,
+            "lat": lat,
+            "lng": lng
+        })
+    return busstops
+
+def get_system_from_route_html(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+
+    # h2 タグのテキストを取得
+    target_h2 = soup.find("div", class_="hGroup201").find("h2")
+    text = target_h2.get_text(strip=True) if target_h2 else ""
+
+    # 正規表現で「漢字1文字＋数字1〜2桁」を抽出
+    match = re.search(r"[一-龯]{1}\d{1,2}", text)
+    if match:
+        return match.group()
+    else:
+        return None
+
+def update_route_ids_list2():
+    base_locations = [
+        {"sdid": "00025625", "name": '町田バスセンター'},
+    ]
+    processed_sdid = set()
+    queue = deque(base_locations)
+    all_route_ids = set()
+    queue_idx = 0
+    while queue:
+        base_location = queue.popleft()
+        sdid = base_location["sdid"]
+        name = base_location["name"]
+        queue_idx += 1
+        print(f"[{queue_idx}/{len(queue)}] process sdid={sdid}, name={name}")
+        route_ids = get_route_ids_list(sdid, name)
+        all_route_ids.update(route_ids)
+        for route_id in route_ids:
+            busstops = get_busstops_list(route_id)
+            for busstop in busstops:
+                new_sdid = busstop["id"]
+                new_name = busstop["name"]
+                if new_sdid not in processed_sdid:
+                    processed_sdid.add(new_sdid)
+                    queue.append({"sdid": new_sdid, "name": new_name})
+                    print(f"    append sdid={new_sdid}, name={new_name}")
+    route_ids_path = os.path.join(os.path.dirname(__file__), "route_ids.json")
+    with open(route_ids_path, 'w', encoding='utf-8') as f:
+        json.dump(sorted(all_route_ids), f, indent=4, ensure_ascii=False)
+
 def update_route_ids_list():
     # route_ids.json を自動生成する関数
     # 既存の route_ids.json を上書きする
@@ -152,8 +247,8 @@ def cleanup_obsolete_route_dirs():
 
 if __name__ == "__main__":
     # 路線IDリストを更新
-    update_route_ids_list()
+    # update_route_ids_list2()
     # 使われなくなった路線ディレクトリを削除
-    cleanup_obsolete_route_dirs()
+    #cleanup_obsolete_route_dirs()
     # 路線ごとにメイン処理を実施
     main()
